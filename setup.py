2150422322ff4b8a8887a71a962cec65db06e5c4
Valentin-Bogdan Roșca <rosca.valentin2012@gmail.com>
2019-10-02 19:18:15 +0300

    Add visual interface and missing deps

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_connect_four',
       version='0.0.1',
+      packages=find_packages(),
       install_requires=['gym>=0.14',
-                        'numpy>=1.17',
+                        'numpy>=1.17.0',
+                        'tensorflow>=1.10.0',
+                        'scikit-image==0.14.5',
                         'keras',
                         'h5py']
       )
